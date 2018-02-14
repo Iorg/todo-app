0f219c060b499863623541921fb88a05b46a9f97
AnnaKira <mascarad33@gmail.com>
2018-02-14 22:40:53 +0300

    Add ex 5 and 6

---
+++ exercises/ex5.js
@@ -0,0 +1,26 @@
+var someProduct = { 
+    name: "Handcrafted Granite Shoes", 
+    price: 269.0 
+};
+
+var anotherProduct = { 
+    name: "Refined Fresh Computer", 
+    price: 134.0 
+};
+
+// Return array of names of both products
+function testForAnna(product1, product2) {
+
+}
+
+// Return total price of both products
+function testForZombieSean(product1, product2) {
+
+}
+
+var annaResult = testForAnna(someProduct);
+
+var zombieSeanResult = testForZombieSean(someProduct);
+
+console.log(annaResult);
+console.log(zombieSeanResult);
---
+++ exercises/ex6.js
@@ -0,0 +1,84 @@
+// Hi there
+
+var testArray = [
+    {
+        name: "Rustic Wooden Pizza",
+        price: 479.00,
+        color: "orange"
+    },
+    {
+        name: "Small Wooden Pants",
+        price: 617.00,
+        color: "black"
+    },
+    {
+        name: "Rustic Soft Salad",
+        price: 574.00,
+        color: "orange"
+    },
+    {
+        name: "Unbranded Soft Sausages",
+        price: 553.00,
+        color: "pink"
+    },
+    {
+        name: "Gorgeous Steel Chair",
+        price: 482.00,
+        color: "black"
+    },
+    {
+        name: "Refined Plastic Bacon",
+        price: 252.00,
+        color: "orange"
+    },
+    {
+        name: "Incredible Plastic Shoes",
+        price: 776.00,
+        color: "black"
+    },
+    {
+        name: "Unbranded Rubber Pants",
+        price: 230.00,
+        color: "pink"
+    },
+    {
+        name: "Fantastic Concrete Hat",
+        price: 489.00,
+        color: "orange"
+    },
+    {
+        name: "Generic Concrete Chicken",
+        price: 956.00,
+        color: "pink"
+    }
+];
+
+// Return array of names of both products
+function testForAnna(products) {
+    var array;
+    for (var i = 0; i < products.length; i++) {
+        var product = products[i];
+    }
+
+    return array;
+}
+
+// Return total price of all products
+function testForZombieSean(product1s) {
+    var price;
+    for (var i = 0; i < products.length; i++) {
+        var product = products[i];
+        
+    }
+
+    return price;
+}
+
+// Calculate sum of length of elements of the array.
+
+var annaResult = testForAnna(testArray);
+
+var zombieSeanResult = testForZombieSean(testArray);
+
+console.log(annaResult);
+console.log(zombieSeanResult);
